ad2c3a75b35be5ea2e4a0d19fda85369feb6b473
Alejandro Cabrera <101005998+Alek30k@users.noreply.github.com>
2026-04-24 08:19:20 -0300

    Update image path for product 'Letra A'

--- app/data/products.ts
+++ app/data/products.ts
@@ -62,7 +62,7 @@ export const products = [
   {
     id: 7,
     name: "Letra A",
-    images: ["/images/llavero1.1.png"],
+    images: ["/images/llavero1.2.png"],
     description: "Llavero con letra A, personalizable con tu nombre",
     price: 2800,
     category: "letras",
