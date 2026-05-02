81ed735750a1bd5ebe504b4ffd1d77541f89afd6
Alek30k <alemank23k@gmail.com>
2026-05-02 18:06:12 -0300

    last commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
       initial={{ opacity: 0, y: -25 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 1, delay: 2.2 }}
-      className="fixed top-0 left-0 w-full z-50 bg-black/30 backdrop-blur-xl border-b border-white/5"
+      className=" left-0 w-full z-50 bg-black/30 backdrop-blur-xl border-b border-white/5"
     >
       <div className="max-w-7xl mx-auto px-5 md:px-10 py-4 flex items-center justify-between">
         <h1 className="text-sm md:text-base tracking-[4px] font-light text-white uppercase">
